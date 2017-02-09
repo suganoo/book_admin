688bca2349fa121a579f90e02d6e083761f2e31a
菅沼 嘉一 <suganoo2002@gmail.com>
2017-02-09 05:49:49 +0000

    first commit

---
+++ app/models/book.rb
@@ -0,0 +1,25 @@
+class Book < ActiveRecord::Base
+  scope :costly, -> { where("price > ?", 3000) }
+  scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
+
+  belongs_to :publisher
+
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 15 }
+  validates :price, numericality: { greater_than_or_equal_to:0 }
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
+
+  before_validation :add_lovely_to_cat
+  def add_lovely_to_cat
+    book.name = book.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
+end
